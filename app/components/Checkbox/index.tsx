668d11ee7ad082cfcb16c6dd4062701aead088b8
Thais Martins <thaismartins1999@gmail.com>
2024-03-12 20:10:51 -0300

    feat: add class-variance-authority and update Checkbox component

--- app/components/Checkbox/index.tsx
+++ app/components/Checkbox/index.tsx
@@ -1,26 +1,83 @@
+import { VariantProps, cva, cx } from "class-variance-authority";
 import React from "react";
 
-interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+const inputStyle = cva(
+  [
+    "peer",
+    "relative",
+    "appearance-none",
+    "w-4",
+    "h-4",
+    "border-2",
+    "border-gray-600",
+    "rounded-sm",
+    "mt-1",
+    "bg-white",
+    "cursor-pointer",
+    "focus:outline-none",
+    "focus:ring-offset-0",
+    "focus:ring-2",
+    "focus:ring-gray-400",
+    "focus:checked:ring-blue-700",
+    "hover:bg-gray-200",
+    "hover:checked:bg-blue-600",
+    "checked:bg-blue-500",
+    "checked:border-0",
+  ],
+  {
+    variants: {
+      size: {
+        small: ["h-5", "w-5"],
+        medium: ["h-6", "w-6"],
+        large: ["h-7", "w-7"],
+      },
+    },
+    defaultVariants: {
+      size: "medium",
+    },
+  }
+);
+
+const svgStyle = cva([
+  "absolute",
+  "pointer-events-none",
+  "hidden",
+  "peer-checked:block",
+  "stroke-white",
+  "mt-1",
+  "pl-1",
+  "outline-none",
+], {
+  variants: {
+    size: {
+      small: ["w-4", "h-4"],
+      medium: ["w-5", "h-5"],
+      large: ["w-6", "h-6"],
+    },
+  },
+  defaultVariants: {
+    size: "medium",
+  },
+  
+})
+
+interface CheckboxProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'>, VariantProps<typeof inputStyle> {
   label: string;
+  hoverBackground?: boolean;
 }
 
-export default function Checkbox({ label, ...props }: CheckboxProps) {
+export default function Checkbox({ label, size, hoverBackground, ...props }: CheckboxProps) {
   return (
-    <div className="flex items-center">
+    <div className={cx("flex items-center w-full", {
+      "hover:bg-gray-200": hoverBackground,
+    })}>
       <input
         type="checkbox"
-        id="checkbox"
-        name="checkbox"
-        className="
-            peer relative appearance-none w-4 h-4 border-2 border-gray-600 rounded-sm mt-1 bg-white cursor-pointer
-            focus:outline-none focus:ring-offset-0 focus:ring-1 focus:ring-gray-200 focus:checked:ring-blue-200
-            hover:bg-gray-200 hover:checked:bg-blue-600
-            checked:bg-blue-500 checked:border-0
-          "
+        className={inputStyle({ size })}
         {...props}
       />
       <svg
-        className="absolute w-4 h-4 pointer-events-none hidden peer-checked:block stroke-white mt-1 outline-none"
+        className={svgStyle({ size })}
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="none"
@@ -31,7 +88,10 @@ export default function Checkbox({ label, ...props }: CheckboxProps) {
       >
         <polyline points="20 6 9 17 4 12"></polyline>
       </svg>
-      <label htmlFor="checkbox" className="ml-2 text-gray-700 cursor-pointer">
+      <label
+        htmlFor={props.id}
+        className="ml-2 text-gray-700 cursor-pointer text-xl"
+      >
         {label}
       </label>
     </div>
